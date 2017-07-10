8f0ae5ca3ec9b5b2c00ab3f3a52c026979777307
Mikel Severson <mkseve@gmail.com>
2017-07-09 20:02:27 -0500

    style: rm unused showSearchPage state

--- src/App.js
+++ src/App.js
@@ -5,14 +5,7 @@ import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: true
-  }
+}
 
   render() {
     return (
