8d8cb7df23e8f387c501dc1935036c339fb8dba0
Mikel Severson <mkseve@gmail.com>
2017-08-13 13:42:11 -0500

    feat: finish lesson 2
    learned about redux store, reducers, and actions.

--- lessons/meal-planner/src/index.js
+++ lessons/meal-planner/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import reducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer,
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+ReactDOM.render(<App store={store}/>, document.getElementById('root'));
 registerServiceWorker();
