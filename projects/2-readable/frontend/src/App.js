cd4af902274f32df1f536d7abca53b81a9c56599
Mikel Severson <mkseve@gmail.com>
2017-09-24 16:49:07 -0500

    feat: successful API request

--- projects/2-readable/frontend/src/App.js
+++ projects/2-readable/frontend/src/App.js
@@ -3,6 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: []
+    }
+  }
+
+  componentDidMount() {
+    const url = `http://localhost:3001/categories`;
+    fetch(url, { headers: { 'Authorization': 'authman' } })
+      .then(res => res.json())
+      .then(data => this.setState(data));
+  }
+
   render() {
     return (
       <div className="App">
@@ -12,6 +26,12 @@ class App extends Component {
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          Talking to the backend yields these categories: <br/>
+          {this.state.categories.map((category, index) => {
+            return <li> {category.name} </li>
+          })}
         </p>
       </div>
     );
