91a0fe28282d30fb74feadd2569deb112f77660d
Mikel Severson <mkseve@gmail.com>
2017-09-24 16:50:59 -0500

    chore: template cleanup

--- projects/2-readable/frontend/src/App.js
+++ projects/2-readable/frontend/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -21,12 +20,8 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Readable</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <p>
           Talking to the backend yields these categories: <br/>
           {this.state.categories.map((category, index) => {
