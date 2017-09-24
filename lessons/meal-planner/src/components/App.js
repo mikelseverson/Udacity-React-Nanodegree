5e206d43dc0ee9e5d9a79cdab81d610f687b205a
Mikel Severson <mkseve@gmail.com>
2017-09-24 12:01:21 -0500

    feat: pushing progress

--- lessons/meal-planner/src/components/App.js
+++ lessons/meal-planner/src/components/App.js
@@ -1,47 +1,20 @@
 import React, { Component } from 'react'
-import { addRecipe } from '../actions'
+import {connect} from 'react-redux'
 
 class App extends Component {
-  state = {
-    calendar: null
-  }
-  componentDidMount () {
-    const { store } = this.props
-    
-    store.subscribe(() => {
-      this.setState(() => ({
-        calendar: store.getState()
-      }))
-    })
-  }
-
-  submitFood = () => {
-    this.props.store.dispatch(addRecipe({
-      day: 'monday',
-      meal: 'breakfast',
-      recipe: {
-        label: this.input.value
-      }
-    }))
-    this.input.value = '';
-  }
-
   render() {
+    console.log(this.props);
     return (
       <div>
-        <input
-            type='text'
-            ref={(input) => this.input = input}
-            placeholder="Monday's Breakfast"
-        />
-        <button onClick={this.submitFood}>Submit</button>
-
-        <pre>
-            Monday's Breakfast: {this.state.calendar && this.state.calendar.monday.breakfast}
-        </pre>
-    </div>
+        Hello World
+      </div>
     )
   }
 }
 
-export default App
+function mapStateToComponent (calendar) {
+  return {
+    calendar: 'Mikel'
+  }
+}
+export default connect(mapStateToComponent)(App)
--- lessons/meal-planner/src/index.js
+++ lessons/meal-planner/src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux'
 import reducer from './reducers';
 
 const store = createStore(reducer,
 window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-ReactDOM.render(<App store={store}/>, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
 registerServiceWorker();
