88a83fd56669c0178ba82d1932b434e8cfec073e
Mikel Severson <mkseve@gmail.com>
2017-09-24 20:03:39 -0500

    feat: add routing

--- projects/2-readable/frontend/src/App.js
+++ projects/2-readable/frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
@@ -18,16 +19,41 @@ class App extends Component {
 
   render() {
     return (
+      
       <div className="App">
         <div className="App-header">
           <h2>Welcome to Readable</h2>
+          {/* list of categories */}
+          <p>
+            {
+              /* should link to a category view for that category */
+              this.state.categories
+                .map((category, index) => ' ' +  category.name +  ' ')
+            }
+          </p>
         </div>
-        <p>
-          Talking to the backend yields these categories: <br/>
-          {this.state.categories.map((category, index) => {
-            return <li> {category.name} </li>
-          })}
-        </p>
+        <Route
+          path="/"
+          exact
+          render={() => (<p>hi</p>)}
+        />
+        <Route 
+          path="/category/:name"
+          render={() => null}
+        />
+        <Route 
+          path="/post"
+          render={() => null}
+        />
+        <Route 
+          path="/create/:id"
+          render={() => null}
+        />
+          {/* control for changing the sort method for the list, including at minimum, order by voteScore and order by timestamp */}
+
+          {/* control for adding a new post */}
+
+          {/* List of all posts ordered by vote score */}
       </div>
     );
   }
