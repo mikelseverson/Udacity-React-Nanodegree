724a820344da2d6f189f7139421191731828021a
Mikel Severson <mkseve@gmail.com>
2017-10-23 01:08:16 -0500

    chore: update todos

--- projects/2-readable/frontend/src/components/App.js
+++ projects/2-readable/frontend/src/components/App.js
@@ -33,11 +33,7 @@ import './App.css'
 
 /***TODO***
   POST LIST
-    * Listed posts are displayed with number of comments, current score, and a voting mechanism to upvote or downvote the post. 
-  COMMENTS
-    * Listed comments are displayed with a voting mechanism to upvote or downvote the comment. 
-  COMMENTS / POSTS
-    * The voting mechanism works and correctly displays the new vote score after clicking for both the post and comments.
+    * Listed posts are displayed with number of comments. 
 */
 
 class App extends Component {
