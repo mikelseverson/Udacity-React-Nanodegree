ca178d0eebff2bf94278f5883820eb1664872e01
Mikel Severson <mkseve@gmail.com>
2017-11-05 14:53:51 -0600

    feat(todo): remove todo list

--- projects/3-flashcards/App.js
+++ projects/3-flashcards/App.js
@@ -11,16 +11,7 @@ import CreateDeck from './components/createDeck/createDeck'
 import DeckQuiz from './components/deckQuiz/deckQuiz'
 import DeckView from './components/deckView/deckView'
 import CreateQuestion from './components/createQuestion/createQuestion'
-
-/* Specific Requirements
-* (DONE) Use create-react-native-app to build your project.
-* (DONE) Allow users to create a deck which can hold an unlimited number of cards.
-* Allow users to add a card to a specific deck.
-* The front of the card should display the question.
-* The back of the card should display the answer.
-* Users should be able to quiz themselves on a specific deck and receive a score once they're done.
-* Users should receive a notification to remind themselves to study if they haven't already for that day.
-*/
+import { setLocalNotification } from './utils/helpers'
 
 const RootNavigator = StackNavigator({
   Home: {
@@ -68,6 +59,9 @@ const RootNavigator = StackNavigator({
 );
 
 export default class app extends React.Component {
+  componentDidMount() {
+    setLocalNotification()
+  }
   render() {
     return (
       <Provider store={createStore(reducer, applyMiddleware(thunk))}>
